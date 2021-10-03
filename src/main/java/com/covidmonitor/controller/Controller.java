37610dce27c5543290df0314b958244b96206fdd
Pietro Freire <freiregabriel98@gmail.com>
2021-10-03 09:10:17 -0300

    ✨ Add endpoint apontando para o index.html

--- src/main/java/com/covidmonitor/controller/Controller.java
+++ src/main/java/com/covidmonitor/controller/Controller.java
@@ -1,12 +1,13 @@
 package com.covidmonitor.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index() {
         return "index";
 
