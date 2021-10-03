4620b6b2beca654482792f261490a5944dce022d
Pietro Freire <freiregabriel98@gmail.com>
2021-10-03 20:23:49 -0300

    ✨ Add novos recursos

--- src/main/java/com/covidmonitor/controller/HelloController.java
+++ src/main/java/com/covidmonitor/controller/HelloController.java
@@ -3,14 +3,13 @@ package com.covidmonitor.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("nome", "Mundo");
-		return "hello"; 
+//		model.addAttribute("nome", "Mundo");
+		return "index";
 	}
 }
