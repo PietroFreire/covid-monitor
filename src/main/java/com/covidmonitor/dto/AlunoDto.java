0e8e786ee9d494851088dabd6fa7b253ba4f91be
Pietro Freire <freiregabriel98@gmail.com>
2021-10-03 23:36:23 -0300

    ✨ Add novos recursos

---
+++ src/main/java/com/covidmonitor/dto/AlunoDto.java
@@ -0,0 +1,72 @@
+package com.covidmonitor.dto;
+
+import com.covidmonitor.model.Aluno;
+
+public class AlunoDto {
+
+    protected String nome;
+    protected String senha;
+    protected String data_nascimento;
+    protected String cidade;
+    protected String estado_saude;
+    protected String num_matricula;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado_saude() {
+        return estado_saude;
+    }
+
+    public void setEstado_saude(String estado_saude) {
+        this.estado_saude = estado_saude;
+    }
+
+    public String getNum_matricula() {
+        return num_matricula;
+    }
+
+    public void setNum_matricula(String num_matricula) {
+        this.num_matricula = num_matricula;
+    }
+
+    public Aluno toAluno()  {
+        Aluno aluno = new Aluno(getNome(),getSenha(), getData_nascimento(), getCidade(), getEstado_saude(), getNum_matricula());
+        aluno.setNome(nome);
+        aluno.setSenha(senha);
+        aluno.setData_nascimento(data_nascimento);
+        aluno.setCidade(cidade);
+        aluno.setEstado_saude(estado_saude);
+        aluno.setNum_matricula(num_matricula);
+        return aluno;
+    }
+}
