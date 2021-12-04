f2c5a4396fbac8ff977b1800efe4b7a2256fbcc1
Pietro Freire <freiregabriel98@gmail.com>
2021-12-03 22:49:56 -0300

    Atualização

--- src/main/java/com/covidmonitor/controller/AutenticacaoController.java
+++ src/main/java/com/covidmonitor/controller/AutenticacaoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-public class Autenticacao {
+public class AutenticacaoController {
 
     @Autowired
     protected MembroRepo membroRepo;
